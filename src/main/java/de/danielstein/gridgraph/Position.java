c73c69706de0fe9e0bd1f4ffcc4110e4868572be
Daniel Stein <tbo007@gmx.de>
2024-05-19 12:27:23 +0200

    Entw. addFakeNotes / Util Methods

---
+++ src/main/java/de/danielstein/gridgraph/Position.java
@@ -0,0 +1,11 @@
+package de.danielstein.gridgraph;
+
+public class Position {
+    public final int layer;
+    public final int row;
+
+    public Position(int layer, int row) {
+        this.layer = layer;
+        this.row = row;
+    }
+}
